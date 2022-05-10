972835f519605545b0b6ea7953f947d6463a7f1e
Your Name <you@example.com>
2022-05-10 12:09:34 +0200

    animate-onclick

--- frontend/script.js
+++ frontend/script.js
@@ -4,13 +4,19 @@ const menuButtonComponent = function () {
 
 const loadEvent = function () {
   const rootElement = document.getElementById("root");
+  console.log(rootElement.parentElement);
 
   rootElement.insertAdjacentHTML("beforeend", menuButtonComponent());
 
   const menuButtonElement = document.getElementById("menu-btn");
 
   menuButtonElement.addEventListener("click", function (event) {
-    event.currentTarget.classList.toggle("clicked");
+    // event.currentTarget.classList.toggle("clicked");
+    // console.log(rootElement);
+    // console.log(event.currentTarget.parentElement);
+    // console.log(event.currentTarget.closest("#root"));
+
+    event.currentTarget.closest("#root").classList.toggle("menu-opened");
   });
 };
 window.addEventListener("load", loadEvent);
